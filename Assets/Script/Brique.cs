59fb4fc13c9bb8358b8bd64c1bc0814b69d5c77d
AshiyroMisachi <riallot.alexandre@outlook.fr>
2024-01-22 16:00:35 +0100

    Bug Tetris

--- Assets/Script/Brique.cs
+++ Assets/Script/Brique.cs
@@ -16,7 +16,7 @@ public class Brique : MonoBehaviour
     public void OnCollisionEnter2D(Collision2D collision)
     {
         Brique brique = collision.gameObject.GetComponent<Brique>();
-        if (brique != null && brique.transform.parent != transform.parent)
+        if (brique != null && brique.transform.parent != transform.parent && transform.parent == tetrisManager.currentTetrominos)
         {
             tetrisManager.CreateTetrominos();
         }
@@ -24,7 +24,12 @@ public class Brique : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        tetrisManager.currentTetrominos.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        tetrisManager.CreateTetrominos();
+        if (tetrisManager.timerMovementValue[9])
+        {
+            tetrisManager.currentTetrominos.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            tetrisManager.CreateTetrominos();
+            StartCoroutine(tetrisManager.ResetTimerMovement(9));
+        }
+
     }
 }
